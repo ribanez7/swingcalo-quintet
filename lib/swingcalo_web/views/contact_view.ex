c400ed1c8b91ee0011bf61f396f725f919f14887
Rubén Ibáñez Carmona <ruben.ibanez@getapp.com>
2019-02-11 17:13:58 +0100

    uniq assets per page

--- lib/swingcalo_web/views/contact_view.ex
+++ lib/swingcalo_web/views/contact_view.ex
@@ -1,3 +1,18 @@
 defmodule SwingcaloWeb.ContactView do
   use SwingcaloWeb, :view
+
+  def uniq_css(conn) do
+    """
+    <link rel="stylesheet" href="#{Routes.static_path(conn, "/css/main_styles.css")}"/>
+    <link rel="stylesheet" href="#{Routes.static_path(conn, "/css/responsive.css")}"/>
+    """
+    |> raw()
+  end
+
+  def uniq_js(conn) do
+    """
+    <script type="text/javascript" src="#{Routes.static_path(conn, "/js/custom.js")}"></script>
+    """
+    |> raw()
+  end
 end
